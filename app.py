930c8be23739cdf13c2a306bb388cd6036b99a6d
Arif <arif.rahman@wcma.vic.gov.au>
2025-08-22 14:55:08 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def root():
+    return send_from_directory("static", "index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
